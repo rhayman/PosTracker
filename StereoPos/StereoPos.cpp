2dfc57990df7c1ba27b31e11c1e32c3c3b43147f
Robin Hayman <r.hayman@ucl.ac.uk>
2019-02-25 12:33:23 +0000

    removed bad tracked points from video display

--- StereoPos/StereoPos.cpp
+++ StereoPos/StereoPos.cpp
@@ -26,7 +26,7 @@ public:
 				cv::waitKey(1);
 			}
 			bool found = false;
-			found = cv::findChessboardCorners(grey, board_size, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+			found = cv::findChessboardCorners(grey, board_size, corners, 1 | 4);
 			if ( found ) {
 				std::cout << "Got a chess board" << std::endl;
 				cv::cornerSubPix(grey, corners, cv::Size(5,5), cv::Size(-1,-1), cv::TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 30, 0.1));
