d7c89396f4932daa131210852b7e65b4679a8ed3
Robin Hayman <r.hayman@ucl.ac.uk>
2019-02-28 11:09:44 +0000

    improving chessboard detection

--- StereoPos/StereoPos.cpp
+++ StereoPos/StereoPos.cpp
@@ -148,7 +148,7 @@ void StereoPos::run() {
 					std::cout << "Saving images after " << std::difftime(nowtime, starttime) << " seconds" << std::endl;
 					if ( tracker->isStreaming() ) {
 						void * frame_ptr = tracker->get_frame_ptr();
-						frame = static_cast<cv::Mat>(&frame_ptr);
+						frame = static_cast<cv::Mat>(frame_ptr*);
 					}
 					images[i].push_back(frame);
 					cv::imshow("capture_" + std::to_string(i), frame);
