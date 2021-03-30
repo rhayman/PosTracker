6d59f312242f9a0da029a128838bc05e21114119
Robin Hayman <r.hayman@ucl.ac.uk>
2021-03-30 17:34:26 +0100

    reduced number of plugins to 1

--- OpenEphysLib.cpp
+++ OpenEphysLib.cpp
@@ -23,8 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <PluginInfo.h>
 #include "PosTracker/PosTracker.h"
-#include "StereoPos/StereoPos.h"
-#include "cvTracking/Trackers.hpp"
 #include <string>
 #ifdef WIN32
 #include <Windows.h>
@@ -35,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using namespace Plugin;
 //Number of plugins defined on the library. Can be of different types (Processors, RecordEngines, etc...)
-#define NUM_PLUGINS 3
+#define NUM_PLUGINS 1
 
 extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
@@ -56,18 +54,6 @@ extern "C" EXPORT int getPluginInfo(int index, Plugin::PluginInfo* info)
 		info->processor.name = "Pos Tracker"; //Processor name shown in the GUI
 		info->processor.type = Plugin::SourceProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
 		info->processor.creator = &(Plugin::createProcessor<PosTracker>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
-		break;
-	case 1:
-		info->type = Plugin::PLUGIN_TYPE_PROCESSOR; //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
-		info->processor.name = "Stereo Pos"; //Processor name shown in the GUI
-		info->processor.type = Plugin::FilterProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
-		info->processor.creator = &(Plugin::createProcessor<StereoPos>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
-		break;
-	case 2:
-		info->type = Plugin::PLUGIN_TYPE_PROCESSOR; //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
-		info->processor.name = "Tracking API"; //Processor name shown in the GUI
-		info->processor.type = Plugin::FilterProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
-		info->processor.creator = &(Plugin::createProcessor<Trackers>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
 		break;
 	default:
 		return -1;
